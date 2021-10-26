adca08b77c3d82a0cce9cdd59b2fa310b405aecd
Mads92 <madskoie@hotmail.com>
2021-10-26 19:55:40 +0200

    Update KanyeWestBot.py

--- KanyeWestBot.py
+++ KanyeWestBot.py
@@ -1,2 +1,28 @@
 #KanyeWestBot
+import requests
+from requests import api
+import json
 
+
+Greeting = "Yo im Yeezy!"
+
+print(Greeting)
+
+API = "https://api.kanye.rest/"
+
+test_api = requests.get("https://api.kanye.rest/")
+kanye_quote = test_api.json()
+print(kanye_quote['quote'])
+
+quotes = []
+
+for i in range(10):
+    test_api = requests.get("https://api.kanye.rest/")
+    kanye_quote = test_api.json()
+    quotes.append(kanye_quote['quote'])
+
+print(quotes)
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
